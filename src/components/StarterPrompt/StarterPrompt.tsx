36c002b0f626cd54a708e1f66d5250701b771305
tommydemarco <tommaso.demarco95@gmail.com>
2021-05-15 23:53:43 +0100

    Adds toggling functionality to StarterPrompt component

--- src/components/StarterPrompt/StarterPrompt.tsx
+++ src/components/StarterPrompt/StarterPrompt.tsx
@@ -25,16 +25,16 @@ const StarterPrompt: React.FC<StarterPromptProps> = ({ active, setPrompt }) => {
         <h4 className="starter-prompt__title">Select a difficulty</h4>
         <p className="starter-prompt__subtitle">You can set a global difficulty in your profile settings.</p>
         <div className="starter-prompt__choice">
-          <span className="starter-prompt__option">
+          <span className="starter-prompt__option" onClick={() => setSeniorDifficulty(false)}>
             Junior
           </span>
           <input type="checkbox" className="starter-prompt__toggle" onChange={() => setSeniorDifficulty(!seniorDifficulty)} checked={seniorDifficulty} />
-          <span className="starter-prompt__option">
+          <span className="starter-prompt__option" onClick={() => setSeniorDifficulty(true)}>
             Senior
           </span>
         </div>
         <div className="starter-prompt__cta-wrapper">
-          <IonButton className="ion-activatable ripple-parent" fill="outline" expand="block">
+          <IonButton onClick={() => setPrompt(false)} className="ion-activatable ripple-parent" fill="outline" expand="block">
             Go Back
           </IonButton>
           <IonButton className="ion-activatable ripple-parent" expand="block">
