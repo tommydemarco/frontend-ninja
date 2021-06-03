7d3de3b609a6746e4da13742f541c3542c376c70
tommydemarco <tommaso.demarco95@gmail.com>
2021-06-03 23:17:45 +0100

    IMplements logic in EditProfileModal component

--- src/components/EditProfileModal/EditProfileModal.tsx
+++ src/components/EditProfileModal/EditProfileModal.tsx
@@ -1,7 +1,26 @@
-import { IonButton, IonButtons, IonHeader, IonIcon, IonModal, IonTitle, IonToolbar } from "@ionic/react";
+import { useState } from "react";
+import { 
+  IonButton, 
+  IonButtons, 
+  IonHeader, 
+  IonIcon, 
+  IonInput, 
+  IonItem, 
+  IonLabel, 
+  IonModal, 
+  IonTextarea, 
+  IonTitle, 
+  IonToolbar,
+  IonSelect,
+  IonRow,
+  IonSelectOption, 
+  IonToggle,
+  IonItemDivider,
+  IonCol} from "@ionic/react";
 import { close } from "ionicons/icons";
 
 import ContentContainer from "../ContentContainer";
+import DoubleCTA from "../DoubleCTA";
 
 import "./EditProfileModal.scss"
 
@@ -12,12 +31,20 @@ interface EditProfileModalProps {
 
 const EditProfileModal: React.FC<EditProfileModalProps> = ({ isOpen, setIsOpen }) => {
 
+  const [ selectedStack, setSelectedStack ] = useState<string[]>([]);
+  const [ privateProfile, setPrivateProfile ] = useState(false);
+  const [ difficulty, setDifficulty ] = useState("always ask");
+
+  const saveChanges = () => {
+    //add logic for save changes
+  }
+
   return (
     <IonModal isOpen={isOpen}>
       <IonHeader>
         <IonToolbar color="primary">
           <IonTitle>
-            Health Overview
+           Profile Settings
           </IonTitle>
           <IonButtons slot="end">
             <IonButton onClick={() => setIsOpen(false)}>
@@ -27,7 +54,74 @@ const EditProfileModal: React.FC<EditProfileModalProps> = ({ isOpen, setIsOpen }
         </IonToolbar>
       </IonHeader>
       <ContentContainer>
-        <h3>Edit Profile</h3>
+        <h3>Edit your Profile</h3>
+        <IonItemDivider className="ion-padding-top">
+          Profile information
+        </IonItemDivider>
+        <IonItem>
+          <IonLabel>
+            Username
+          </IonLabel>
+          <IonInput type="text" />
+        </IonItem>
+        <IonItem>
+          <IonLabel>
+            Profile info
+          </IonLabel>
+          <IonTextarea />
+        </IonItem>
+        <IonItem>
+          <IonLabel>Your Stack</IonLabel>
+          <IonSelect 
+            value={selectedStack} 
+            multiple={true} 
+            okText="Select" 
+            cancelText="Dismiss" 
+            onIonChange={e => setSelectedStack(e.detail.value)}
+          >
+            <IonSelectOption value="brown">Brown</IonSelectOption>
+            <IonSelectOption value="blonde">Blonde</IonSelectOption>
+            <IonSelectOption value="black">Black</IonSelectOption>
+            <IonSelectOption value="red">Red</IonSelectOption>
+          </IonSelect>
+        </IonItem>
+        <IonItemDivider className="ion-padding-top">
+          General settings
+        </IonItemDivider>
+        <IonItem>
+          <IonLabel>
+            Private Profile
+          </IonLabel>
+          <IonToggle 
+            checked={privateProfile} 
+            onIonChange={e => setPrivateProfile(e.detail.checked)}
+          />
+        </IonItem>
+        <IonItem>
+          <IonLabel>Difficulty</IonLabel>
+          <IonSelect 
+            value={difficulty} 
+            okText="Select" 
+            cancelText="Dismiss" 
+            onIonChange={e => setDifficulty(e.detail.value)}
+          >
+            <IonSelectOption value="always ask">Always ask</IonSelectOption>
+            <IonSelectOption value="blonde">Junior</IonSelectOption>
+            <IonSelectOption value="black">Senior</IonSelectOption>
+          </IonSelect>
+        </IonItem>
+        <IonRow className="ion-padding-top ion-no-margin">
+          <IonCol size="12">
+            <DoubleCTA>
+              <IonButton onClick={() => setIsOpen(false)} fill="outline">
+                Go back
+              </IonButton>
+              <IonButton onClick={saveChanges}>
+                Save changes
+              </IonButton>
+            </DoubleCTA>
+          </IonCol>
+        </IonRow> 
       </ContentContainer>
     </IonModal>
   )
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,51 +1,59 @@
+import React, { useState } from "react";
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButtons, IonMenuButton, IonButton } from '@ionic/react';
-import { create } from "ionicons/icons";
+import { settings } from "ionicons/icons";
 
 import Card from "../components/Card"
 import ContentContainer from "../components/ContentContainer";
-import DoubleCTA from "../components/DoubleCTA"
+import DoubleCTA from "../components/DoubleCTA";
+import EditProfileModal from "../components/EditProfileModal";
 import ProfileInfo from "../components/ProfileInfo";
 import Records from "../components/Records";
 import TagContainer from "../components/TagContainer";
 
-import { profileData } from "../data/profile-data"
+import { profileData } from "../data/profile-data";
 
 const Tab3: React.FC = () => {
+
+  const [ modalOpen, setModalOpen ] = useState(false);
+
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar color="primary">
-          <IonTitle>Profile</IonTitle>
-          <IonButtons slot="end">
-            <IonMenuButton/>
-          </IonButtons>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <ContentContainer>
-          <Card 
-            handlerFunction={() => console.log("clicked")} 
-            buttonIcon={create} 
-            buttonText="edit profile"
-          >
-            <ProfileInfo {...profileData}>
-              <TagContainer tags={profileData.stack} />
-            </ProfileInfo>
-          </Card>
-          <Card>
-            <Records quizzesRecord={1879} sniperRecord={1879} timeRecord={1879} bugfixRecord={1879} />
-          </Card>
-          <DoubleCTA>
-            <IonButton color="danger">
-              Delete profile
-            </IonButton>
-            <IonButton routerLink="/logout">
-              Logout
-            </IonButton>
-          </DoubleCTA>
-        </ContentContainer>
-      </IonContent>
-    </IonPage>
+    <React.Fragment>
+      <IonPage>
+        <IonHeader>
+          <IonToolbar color="primary">
+            <IonTitle>Profile</IonTitle>
+            <IonButtons slot="end">
+              <IonMenuButton/>
+            </IonButtons>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent fullscreen>
+          <ContentContainer>
+            <Card 
+              handlerFunction={() => setModalOpen(true)}
+              buttonIcon={settings} 
+              buttonText="profile settings"
+            >
+              <ProfileInfo {...profileData}>
+                <TagContainer tags={profileData.stack} />
+              </ProfileInfo>
+            </Card>
+            <Card>
+              <Records quizzesRecord={1879} sniperRecord={1879} timeRecord={1879} bugfixRecord={1879} />
+            </Card>
+            <DoubleCTA>
+              <IonButton color="danger">
+                Delete profile
+              </IonButton>
+              <IonButton routerLink="/logout">
+                Logout
+              </IonButton>
+            </DoubleCTA>
+          </ContentContainer>
+        </IonContent>
+      </IonPage>
+      <EditProfileModal isOpen={modalOpen} setIsOpen={setModalOpen} />
+    </React.Fragment>
   );
 };
 
