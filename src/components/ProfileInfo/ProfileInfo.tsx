b6f65d1f7098b935660223256f5bef856caedcaa
tommydemarco <tommaso.demarco95@gmail.com>
2021-06-04 22:53:19 +0100

    Integrate EditAvatarModal into ProfileInfo component

--- src/components/ProfileInfo/ProfileInfo.tsx
+++ src/components/ProfileInfo/ProfileInfo.tsx
@@ -1,7 +1,10 @@
+import React, { useState } from "react";
 import { IonButton, IonCol, IonGrid, IonIcon, IonRow } from "@ionic/react";
 import { create } from "ionicons/icons";
-import React from "react";
-import "./ProfileInfo.scss"
+
+import EditAvatarModal from "../EditAvatarModal";
+
+import "./ProfileInfo.scss";
 
 interface ProfileInfoProps {
   imageSrc: string;
@@ -12,30 +15,36 @@ interface ProfileInfoProps {
 }
 
 const ProfileInfo: React.FC<ProfileInfoProps> = ({ imageSrc, imageAlt, username, description, children }) => {
+
+  const [ editAvatarOpen, setEditAvatarOpen ] = useState(false)
+
   return (
-    <div className="profile-info">
-      <IonGrid>
-        <IonRow className="profile-info__initial-info">
-          <IonCol size="6" className="profile-info__image-container">
-            <img src={imageSrc} alt={imageAlt} className="profile-info__image" />
-            <IonButton className="profile-info__edit-avatar">
-              <IonIcon icon={create} slot="icon-only" />
-            </IonButton>
-          </IonCol>  
-          <IonCol size="6" className="profile-info__data">
-            <h3 className="profile-info__username">
-              {username}
-            </h3>
-            {children}
-          </IonCol>
-        </IonRow>
-        <IonRow>
-          <span className="profile-info__description">
-            {description}
-          </span>
-        </IonRow>
-      </IonGrid>   
-    </div>
+    <React.Fragment>
+      <div className="profile-info">
+        <IonGrid>
+          <IonRow className="profile-info__initial-info">
+            <IonCol size="6" className="profile-info__image-container">
+              <img src={imageSrc} alt={imageAlt} className="profile-info__image" />
+              <IonButton className="profile-info__edit-avatar">
+                <IonIcon icon={create} slot="icon-only" />
+              </IonButton>
+            </IonCol>  
+            <IonCol size="6" className="profile-info__data">
+              <h3 className="profile-info__username">
+                {username}
+              </h3>
+              {children}
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <span className="profile-info__description">
+              {description}
+            </span>
+          </IonRow>
+        </IonGrid>   
+      </div>
+      <EditAvatarModal isOpen={editAvatarOpen} setIsOpen={() => setEditAvatarOpen(false)} />
+    </React.Fragment>
   )
 }
 
